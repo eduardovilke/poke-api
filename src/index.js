b4bc014507a7e6bef92fe196142f71694f637600
Eduardo Vilke <eduardo@twoweb.com.br>
2022-03-29 00:17:38 -0300

    set port an host

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import cors from 'cors'
 
 import { router } from './routes/index.js'
 
+const PORT = 3333
+const HOST = '0.0.0.0'
+
 const app = express()
+
 app.use(express.json())
 app.use(cors())
 
 app.use('/api', router)
 
-app.listen(3333, () => console.log("Server is running! 🚀"))
+app.listen(PORT, HOST, () => console.log("Server is running! 🚀"))
